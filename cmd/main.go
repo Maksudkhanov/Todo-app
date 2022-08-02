a71b42ac2a8806721799c360f2115f4439b1b0e4
Maksudkhanov <azik.maksudxanov.am@gmail.com>
2022-08-02 10:04:00 +0500

    added repo and service

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"github/maksudkhanov/todo-app"
 	"github/maksudkhanov/todo-app/pkg/handler"
+	"github/maksudkhanov/todo-app/pkg/repository"
+	"github/maksudkhanov/todo-app/pkg/service"
 	"log"
 )
- 
+
 func main() {
-	handler:=new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("3000", handler.InitRoutes()); err != nil {
+	if err := srv.Run("3000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while runing server: %s", err.Error())
 	}
 }
